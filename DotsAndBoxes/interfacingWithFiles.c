9a5bde7014646604d22a6fc48552a931add6e1e9
BerryYoghurt <58439150+BerryYoghurt@users.noreply.github.com>
2019-12-03 22:53:34 +0200

    Add files via upload

---
+++ DotsAndBoxes/interfacingWithFiles.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "interfacingWithFiles.h" //to be added
+
+void savePlayer(/*args*/){
+
+    //write record of player after the game has finished
+
+}
+
+void saveGame(/*args*/){
+
+    //write the current game into a file with all the in-game info
+}
+
+void loadGame(/*args*/){
+
+    //load an EXISTING game from a file
+}
+
+struct palyer * getTop10(/*args*/){
+
+    //returns an array of top 10 players
+}
+
---
+++ DotsAndBoxes/structs.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "structs.h" //to be added
+struct player{
+
+    char name[22]; //name can be at most 20 characters + 1 NULL + 1 \n check if pdcurses input puts them automatically
+    int score;
+    int color; //see pdcurses interface DOES it have to be int??
+};
+
+struct dot{
+
+    unsigned char up;//stores whether the dot is connected to another dot. if yes, stores the index of the line used
+    unsigned char down;
+    unsigned char left;
+    unsigned char right;
+    int position; //check pdcurses for precise format
+};
+
+struct line{
+
+    struct dot start;
+    struct dot end;
+    int color;
+
+};
